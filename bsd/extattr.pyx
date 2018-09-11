7dcc5ecaca245a622ed6271a34a774e6ad755a72
William Grzybowski <wg@FreeBSD.org>
2018-09-11 14:53:41 -0300

    Fix typo in extattr

--- bsd/extattr.pyx
+++ bsd/extattr.pyx
@@ -335,7 +335,7 @@ def _list(fobj, namespace = Namespaces.USER, follow = True):
             if isinstance(fobj, file_types):
                 kr = defs.extattr_list_file(fobj.fileno(), namespace, data_buffer, nbytes)
             elif type(fobj) is int:
-                kr = defs.exattr_list_file(fobj, namespace, data_buffer, nbytes)
+                kr = defs.extattr_list_file(fobj, namespace, data_buffer, nbytes)
             elif type(fobj) is str:
                 if follow:
                     kr = defs.extattr_list_file(pname, namespace, data_buffer, nbytes)
