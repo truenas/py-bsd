d21dbf5a01e2186f6db4dc1271bc9e780b0ed277
caleb <yocalebo@gmail.com>
2021-10-06 14:35:35 -0400

    add get_ident_with* functions to bsd/disk module

--- bsd/disk.pyx
+++ bsd/disk.pyx
@@ -25,3 +25,25 @@ def get_size_with_file(f):
     _buffer = array('B', range(0, 8))
     ioctl(f.fileno(), disk_ioctl.DIOCGMEDIASIZE, _buffer, 1)
     return unpack('q', _buffer)[0]
+
+
+def get_ident_with_name(disk):
+    """
+    Get ident of disk.
+
+    `disk` must be a string and can be `/dev/da1` or just `da1`
+    """
+    disk = disk.removeprefix('/dev/')
+    with open(f'/dev/{disk}', 'rb') as f:
+        return get_ident_with_file(f)
+
+
+def get_ident_with_file(f):
+    """
+    Get ident of disk.
+
+    `disk` must be a python file object.
+    """
+    _buffer = array('B', range(0, 256))  # max ident size
+    ioctl(f.fileno(), disk_ioctl.DIOCGIDENT, _buffer, 1)
+    return unpack('<256s', _buffer)[0].decode().strip('\x00')
